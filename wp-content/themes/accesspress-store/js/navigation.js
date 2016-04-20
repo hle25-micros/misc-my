5e09348ff61ca39ed496cdfa757ad8df84dc7e55
Hoang <hle25@csc.com>
2016-04-20 14:05:33 +0700

    fix menu tongle

--- wp-content/themes/accesspress-store/js/navigation.js
+++ wp-content/themes/accesspress-store/js/navigation.js
@@ -50,8 +50,35 @@ jQuery('.search-icon').click(function() {
 jQuery('.search-box .close').click(function() {
 	jQuery('.search-box').removeClass('active');
 });
+jQuery(document).ready(function() {
+	//jQuery('#site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="fa fa-angle-right"></i> </span>');
+	jQuery(window).on('load', function() {
 
+			var width = Math.max(window.innerWidth, document.documentElement.clientWidth);
 
+        	 if (width && width <= 687) {
+				jQuery('.menu-toggle,#site-navigation a').click(function() {
+					jQuery('#site-navigation .menu-primary-container,#site-navigation div.menu').slideToggle();
+				});
+				jQuery(".store-menu ul li ul.sub-menu").hide();
+	    	}
+	});
+				jQuery(window).resize(function(){
+					var width = Math.max(window.innerWidth, document.documentElement.clientWidth);
+					console.log(width);
+					if (width && width <= 687) {
+						jQuery(".store-menu ul li ul.sub-menu").hide();	
+					}else{jQuery(".store-menu ul li ul.sub-menu").show();	}
+				});
+   jQuery('#site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="fa fa-angle-right"></i> </span>');
+
+   jQuery('#site-navigation .sub-toggle').click(function() {
+      jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
+      jQuery(this).children('.fa-angle-right').first().toggleClass('fa-angle-down');
+   });
+   
+});	
+/*
 jQuery(function () {
 	jQuery('.store-menu ul li').not(".store-menu ul li ul li").click(function(e){
 		jQuery(".store-menu ul li ul").hide();
@@ -66,4 +93,4 @@ jQuery(function () {
 
 jQuery(document).click(function() {
 	jQuery(".store-menu ul li ul").hide();
-});
+});*/
